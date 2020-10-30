3dcfbb968615560d9b32d0cd06a3af78cc2f0e2d
Creeper <51346607+CreeperPlanet26@users.noreply.github.com>
2020-10-30 18:56:25 -0400

    (fix): Http to http

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ import { HttpEndpoints } from './http-endpoints/http-endpoints.prod';
 
 export const environment = {
   production: true,
-  HttpEndpoints: new HttpEndpoints(),
+  httpEndpoints: new HttpEndpoints(),
 };
