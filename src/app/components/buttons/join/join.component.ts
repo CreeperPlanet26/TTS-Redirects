7e24703198a82cc5a82bcc2633067497de19f7c7
CreeperPlanet26 <fortniteplanet26@gmail.com>
2020-10-30 09:51:34 -0400

    join button works

--- src/app/components/buttons/join/join.component.ts
+++ src/app/components/buttons/join/join.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { inviteLink } from './invite-link';
 
 @Component({
   selector: 'app-join',
   templateUrl: './join.component.html',
   styleUrls: ['./join.component.scss']
 })
-export class JoinComponent implements OnInit {
+export class JoinComponent {
+  public link = inviteLink;
 
   constructor() { }
-
-  ngOnInit(): void {
-  }
 
 }
