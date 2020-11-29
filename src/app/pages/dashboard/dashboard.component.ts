23191e5cfb6179a325ca75e8a3ae0dff6ee0cd66
CreeperPlanet26 <fortniteplanet26@gmail.com>
2020-11-29 18:17:22 -0500

    music change to end at 1 hour  + color effects then

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,17 +1,26 @@
 import { Component } from '@angular/core';
 import { NavService } from 'src/app/components/nav/nav.service';
 
+const Music = {
+  COMIC: 'https://lucas7yoshi.sfo2.cdn.digitaloceanspaces.com/tempogg.ogg.mp3',
+  END: 'assets/Fortnite_The_End_Event_Music.mp3',
+};
+
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent {
+  public music = Music.COMIC;
   public eventDate = new Date('2020-12-01T21:00:00.000Z');
-  // public eventDate = new Date('Nov 23 2020 14:35:20');
   public countdown: string;
+  public class = '';
 
-  constructor(private navBarService: NavService) { this.startUpdating(); }
+  constructor(private navBarService: NavService) {
+    this.startUpdating();
+    console.log(new Date().toISOString());
+  }
 
   public toggleDrawer(): void {
     this.navBarService.drawer.toggle();
@@ -34,14 +43,22 @@ export class DashboardComponent {
       const seconds = Math.floor((msTillEvent % (1000 * 60)) / 1000);
       const milliseconds = Math.floor((msTillEvent % (1000)) / 1000);
 
-      // Return the days, hours, minutes, and seconds all together and formatted.
+      // If there is 1 hour or less to event change music to the end.
+      if (days === 0 && hours <= 1) {
+        this.music = Music.END;
+        this.class = 'orange';
+      }
+
+      // Set the countdown to the days, hours, minutes, and seconds all together and formatted.
       this.countdown = `${days}D ${hours}H ${minutes}M ${seconds}S ${milliseconds}MS`;
 
       // If the count down is over, write some text;
       if (msTillEvent <= 0) {
         clearInterval(interval);
-        this.countdown = 'EXPIRED';
+        this.countdown = 'Galactus has Arrived!';
+        this.class = 'red';
       }
+
 
     }, 1000);
   }
