06f5311268c86b3e52f690e0921d460cbf58d6ff
CreeperPlanet26 <fortniteplanet26@gmail.com>
2020-11-30 11:32:18 -0500

    beta season 5

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { NavService } from 'src/app/components/nav/nav.service';
 
 const Music = {
@@ -12,25 +12,26 @@ const Music = {
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent implements OnInit {
-  public music;
+export class DashboardComponent {
+  public music = Music.COMIC;
   // public eventDate = new Date('2020-12-01T21:00:00.000Z');
   // public endDate = new Date('2020-12-01T21:10:00.000Z');
-  public eventDate = new Date('2020-11-01T21:00:00.000Z');
-  public endDate = new Date('2020-11-01T21:10:00.000Z');
+  public eventDate = new Date('2020-11-30T16:31:00.000Z');
+  public endDate = new Date('2020-11-30T16:32:00.000Z');
   // public season5 = new Date('2020-12-02T09:00:00.000Z');
-  public season5 = new Date();
+  public season5 = new Date('2020-11-30T16:32:10.000Z');
 
-  public countdown: string;
+  public countdown = 'Calculating...';
   public class = '';
   public canShowTilted = false;
+  public canShowEpicGamesCountdown = true;
 
-  constructor(private navBarService: NavService) { }
-
-  public ngOnInit(): void {
-    this.music = Music.COMIC;
+  constructor(private navBarService: NavService) {
+    this.calculate();
     this.startUpdating();
+    console.log(this.eventDate.toLocaleString());
   }
+
 
   public toggleDrawer(): void {
     this.navBarService.drawer.toggle();
@@ -38,54 +39,70 @@ export class DashboardComponent implements OnInit {
 
   public startUpdating(): void {
     // Update the count down every 1 second
-    const interval = setInterval(() => {
+    setInterval(() => {
+      this.calculate();
+    }, 1000);
+  }
 
-      // Get the current milliseconds.
-      const now = new Date().getTime();
+  public calculate(): void {
+    // Get the current milliseconds.
+    const now = new Date().getTime();
 
-      // Find the milliseconds till now and the event.
-      const msTillEvent = this.eventDate.getTime() - now;
-      const msTillEndDate = this.endDate.getTime() - now;
-      const msTillSeason5 = this.season5.getTime() - now;
+    // Find the milliseconds till now and the event.
+    const msTillEvent = this.eventDate.getTime() - now;
+    const msTillEndDate = this.endDate.getTime() - now;
+    const msTillSeason5 = this.season5.getTime() - now;
 
-      // Time calculations for days, hours, minutes and seconds.
-      const days = Math.floor(msTillEvent / (1000 * 60 * 60 * 24));
-      const hours = Math.floor((msTillEvent % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-      const minutes = Math.floor((msTillEvent % (1000 * 60 * 60)) / (1000 * 60));
-      const seconds = Math.floor((msTillEvent % (1000 * 60)) / 1000);
-      const milliseconds = Math.floor((msTillEvent % (1000)) / 1000);
+    // Time calculations for days, hours, minutes and seconds.
+    const days = Math.floor(msTillEvent / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((msTillEvent % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((msTillEvent % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((msTillEvent % (1000 * 60)) / 1000);
+    const milliseconds = Math.floor((msTillEvent % (1000)) / 1000);
 
-      // If there is 1 hour or less to event change music to the end. Also make text orange.
-      if (days === 0 && hours <= 1) {
-        this.music = Music.END;
-        this.class = 'orange';
-      }
+    // If there is 1 hour or less to event change music to the comic. Also make text orange.
+    if (days === 0 && hours <= 1) {
+      this.music = Music.COMIC;
+      this.class = 'orange';
+    }
 
-      // Set the countdown to the days, hours, minutes, and seconds all together and formatted.
+    // Set the countdown to the days, hours, minutes, and seconds all together and formatted.
+    this.countdown = `${days}D ${hours}H ${minutes}M ${seconds}S ${milliseconds}MS`;
+
+    // If the count down is over, write some text;
+    if (msTillEvent <= 0) {
+      this.countdown = '!WARNING! The Devourer of Worlds is in Range';
+      this.class = 'red';
+      this.music = Music.END;
+      this.canShowEpicGamesCountdown = false;
+      this.canShowTilted = false;
+
+    }
+
+    if (msTillEndDate <= 0) {
+      this.countdown = 'To Be Continued...';
+      this.class = 'green';
+      this.music = Music.END;
+      this.canShowEpicGamesCountdown = false;
+      this.canShowTilted = false;
+
+      const days = Math.floor(msTillSeason5 / (1000 * 60 * 60 * 24));
+      const hours = Math.floor((msTillSeason5 % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      const minutes = Math.floor((msTillSeason5 % (1000 * 60 * 60)) / (1000 * 60));
+      const seconds = Math.floor((msTillSeason5 % (1000 * 60)) / 1000);
+      const milliseconds = Math.floor((msTillSeason5 % (1000)) / 1000);
       this.countdown = `${days}D ${hours}H ${minutes}M ${seconds}S ${milliseconds}MS`;
 
-      // If the count down is over, write some text;
-      if (msTillEvent <= 0) {
-        clearInterval(interval);
-        this.countdown = '!WARNING! The Devourer of Worlds is in Range';
-        this.class = 'red';
-      }
+    }
 
-      if (msTillEndDate <= 0) {
-        clearInterval(interval);
-        this.countdown = 'To Be Continued...';
-        this.class = 'blue';
-      }
+    if (msTillSeason5 <= 0) {
 
-      if (msTillSeason5 <= 0) {
-        clearInterval(interval);
-        this.canShowTilted = true;
-        this.music = Music.FESTIVE;
-        this.countdown = 'Welcome to Season 15! Tilted Towers has Returned! (I hope) POGGERS!';
-        this.class = 'blue';
-      }
+      this.music = Music.FESTIVE;
+      this.countdown = 'Welcome to Season 15! Tilted Towers has Returned! (I hope) POGGERS!';
+      this.class = 'blue';
+      this.canShowEpicGamesCountdown = false;
+      this.canShowTilted = true;
+    }
 
-
-    }, 1000);
   }
 }
