6f7df77c0cad5eef10c1c131ec7736b462bdb23c
CreeperPlanet26 <fortniteplanet26@gmail.com>
2020-11-30 10:49:02 -0500

    test

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -4,6 +4,7 @@ import { NavService } from 'src/app/components/nav/nav.service';
 const Music = {
   COMIC: 'https://lucas7yoshi.sfo2.cdn.digitaloceanspaces.com/tempogg.ogg.mp3',
   END: 'assets/Fortnite_The_End_Event_Music.mp3',
+  FESTIVE: 'assets/Fortnite_Festive_Music.mp3',
 };
 
 @Component({
@@ -13,13 +14,19 @@ const Music = {
 })
 export class DashboardComponent {
   public music = Music.COMIC;
-  public eventDate = new Date('2020-12-01T21:00:00.000Z');
+  // public eventDate = new Date('2020-12-01T21:00:00.000Z');
+  // public endDate = new Date('2020-12-01T21:10:00.000Z');
+  public eventDate = new Date('2020-11-01T21:00:00.000Z');
+  public endDate = new Date('2020-11-01T21:10:00.000Z');
+  // public season5 = new Date('2020-12-02T09:00:00.000Z');
+  public season5 = new Date();
+
   public countdown: string;
   public class = '';
+  public canShowTilted = false;
 
   constructor(private navBarService: NavService) {
     this.startUpdating();
-    console.log(new Date().toISOString());
   }
 
   public toggleDrawer(): void {
@@ -35,6 +42,8 @@ export class DashboardComponent {
 
       // Find the milliseconds till now and the event.
       const msTillEvent = this.eventDate.getTime() - now;
+      const msTillEndDate = this.endDate.getTime() - now;
+      const msTillSeason5 = this.season5.getTime() - now;
 
       // Time calculations for days, hours, minutes and seconds.
       const days = Math.floor(msTillEvent / (1000 * 60 * 60 * 24));
@@ -43,7 +52,7 @@ export class DashboardComponent {
       const seconds = Math.floor((msTillEvent % (1000 * 60)) / 1000);
       const milliseconds = Math.floor((msTillEvent % (1000)) / 1000);
 
-      // If there is 1 hour or less to event change music to the end.
+      // If there is 1 hour or less to event change music to the end. Also make text orange.
       if (days === 0 && hours <= 1) {
         this.music = Music.END;
         this.class = 'orange';
@@ -55,8 +64,22 @@ export class DashboardComponent {
       // If the count down is over, write some text;
       if (msTillEvent <= 0) {
         clearInterval(interval);
-        this.countdown = 'Galactus has Arrived!';
+        this.countdown = '!WARNING! The Devourer of Worlds is in Range';
         this.class = 'red';
+      }
+
+      if (msTillEndDate <= 0) {
+        clearInterval(interval);
+        this.countdown = 'To Be Continued...';
+        this.class = 'blue';
+      }
+
+      if (msTillSeason5 <= 0) {
+        clearInterval(interval);
+        this.canShowTilted = true;
+        this.music = Music.FESTIVE;
+        this.countdown = 'Welcome to Season 15! Tilted Towers has Returned! (I hope) POGGERS!';
+        this.class = 'blue';
       }
 
 
