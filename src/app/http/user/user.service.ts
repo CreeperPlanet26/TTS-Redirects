671a9bdda80219f9ba6142692b95378fdd82291d
CreeperPlanet26 <fortniteplanet26@gmail.com>
2020-12-11 10:38:50 -0500

    rm fetch()

--- src/app/http/user/user.service.ts
+++ src/app/http/user/user.service.ts
@@ -13,9 +13,7 @@ export class UserService {
   constructor(private _http: HttpClient) { }
 
   public async getMe(): Promise<User> {
-    // return await this._http.get<User>(Endpoints.ME, { withCredentials: true }).toPromise();
-    // const user = await axios.get<User>(Endpoints.ME, { withCredentials: true });
-    return (await fetch(Endpoints.ME, { method: 'GET', credentials: 'include' }) as unknown as Promise<User>);
+    return await this._http.get<User>(Endpoints.ME, { withCredentials: true }).toPromise();
   }
 
 
